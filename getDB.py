90c4ab39afa9e907d1d370e7b4ad5fec4fb6b8de
Serghei Mangul <serghei@login4.(none)>
2016-09-05 15:42:26 -0700

    release 1.0.5

--- getDB.py
+++ getDB.py
@@ -116,7 +116,7 @@ def symlink(dirDB):
 ####################################################################
 
 print "*********************************************"
-print "ROP (version 1.0.6) is a computational protocol aimed to discover the source of all reads, originated from complex RNA molecules, recombinant antibodies and microbial communities. Written by Serghei Mangul (smangul@ucla.edu) and Harry Taegyun Yang (harry2416@gmail.com), University of California, Los Angeles (UCLA). (c) 2016. Released under the terms of the General Public License version 3.0 (GPLv3)"
+print "ROP (version 1.0.5) is a computational protocol aimed to discover the source of all reads, originated from complex RNA molecules, recombinant antibodies and microbial communities. Written by Serghei Mangul (smangul@ucla.edu), Harry Taegyun Yang (harry2416@gmail.com), Kevin Hsieh(kevin.hsieh@ucla.edu), Linus Chen (u6.30cl@gmail.com), University of California, Los Angeles (UCLA). (c) 2016. Released under the terms of the General Public License version 3.0 (GPLv3)"
 print ""
 print "For more details see:"
 print "https://sergheimangul.wordpress.com/rop/"
