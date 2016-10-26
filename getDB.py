f0708ff9270e44f0cf9bb18b9086137e36fb0464
Kevin Hsieh <kahsieh@login2.(none)>
2016-10-25 21:49:50 -0700

    160824 working

--- getDB.py
+++ getDB.py
@@ -1,39 +1,122 @@
 import sys
-import os 
+import os
 import argparse
+import re
 from subprocess import call, Popen, PIPE
 
+#md5 values
+humandict={}
+humandict['antibody']='37dd2b878d69734fa96fe6f4e182b703'
+humandict['bacteria']='dae3d5d9220b1c40bac72cbf9f710d5c'
+humandict['bowtie2Index']='82241a223bbec89170d1b0c394d48bdd'  
+humandict['BWAIndex']='f9f61c97917adf37a738b31d9888a1a6'  
+humandict['eupathdb']='e38ac558fdcbe4ae85745016712b0e8b' 
+humandict['metaphlan_db']='83e0be082014811189bcb57856d3213d'  
+humandict['repeats']='132b9419746ea542eab966c06b090587'  
+humandict['rRNA']='93756de9ed7a6f0488cdebf7e5a3eb93'
+humandict['virus']='02a7c899924ce6bac64472abeade2e6d'  
 
-#codeDir
+mousedict={}
+mousedict['antibody']='5fcbd7d0bfcf534317b2643b1aa7ee3d'
+mousedict['bacteria']='5dd33ee0456bbbca077f13338d1fdbbd'
+mousedict['bowtie2Index']='e8437ca0f382ca41c86f3b8c8c290999'
+mousedict['BWAIndex']='915b3673f970f3045a2410860589839f'
+mousedict['eupathdb']='ea35b8e758df4939540c8540bed4e7c4'
+mousedict['metaphlan_db']='2ecd61ee68065903011446e37fecc186'
+mousedict['repeats']='fc4c643475ce8b6c74136a6b2018b445'
+mousedict['rRNA']='c6f70df0fcd70ae14e1963301644e134'
+mousedict['virus']='02a7c899924ce6bac64472abeade2e6d'
+
+dict={}
+dict["mouse"]=mousedict
+dict["human"]=humandict
+
+humanlinks={}
+humanlinks['antibody']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOWWJlVks0UFpvRVk"]
+humanlinks['bacteria']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOdWt6NzU4TGZONjA"]
+humanlinks['bowtie2Index']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOLTRfV25DTnEzY1E"]
+humanlinks['BWAIndex']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cORnBPd20wb2JrVFU"]
+humanlinks['eupathdb']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOSTVQaTY4QktpSnc"]
+humanlinks['metaphlan_db']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOVUVkcFNjdU5BbXM"]
+humanlinks['repeats']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOYWxkQ0ZwNGE2dVU"]
+humanlinks['rRNA']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOUWkyTWZEcUxNT2M"]
+humanlinks['virus']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cObXVfMDd2aENNbms"]
+
+mouselinks={}
+
+mouselinks['antibody']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOSzJxWm5obXBtc3c"]
+mouselinks['bacteria']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOQTdRRWhXN245OGM"]
+mouselinks['bowtie2Index']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOTWNYTnhObUdObWs"]
+mouselinks['BWAIndex']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOR083NnNackYzdWM"]
+mouselinks['eupathdb']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOUHJHWUZFYzZoM1k"]
+mouselinks['metaphlan_db']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOeFA2bi02RlRCMGc"]
+mouselinks['repeats']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOWVFPZGhDd0g2SUk"]
+mouselinks['rRNA']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cOR0kzeWVtS1Q4MkU"]
+mouselinks['virus']=["https://drive.google.com/uc?export=download","&id=0Bx1fyWeQo3cObXVfMDd2aENNbms"]
+
+links={}	
+links["human"]=humanlinks
+links["mouse"]=mouselinks
+
+#codeDir: the directory which contains this file
 codeDir=os.path.dirname(os.path.realpath(__file__))
 
-def download(dbName,md5sum,dirDB):
+def download(dbName,dbType,dirDB):
+	"""change directory to dirDB, and download dbName to it. Check that the 
+	md5sum matches. dbName must be found on the server as a .tar.gz, but this
+	command will extract and then delete the tar."""
+	
+	tarName=dirDB+"/"+"%s.tar.gz" %(dbName)
+	link=links[dbType][dbName][0] + links[dbType][dbName][1]
+	md5sum=dict[dbType][dbName]
+	os.chdir(dirDB)
+	print "-----------------------------"
+	print "Attempting to download %s." %(link)
+	if dbName in ['rRNA', 'antibody', 'repeats']:
+		getting = Popen(["wget", link, "--output-document="+tarName, "--no-check-certificate"])
+		if getting.wait():
+			print "problem downloading online database."
 
-    os.chdir(dirDB)
-    tarName=dirDB+"/"+"%s.tar.gz" %(dbName)
-    link="https://googledrive.com/host/0Bx1fyWeQo3cOMjFNMzBrcWZfXzA/%s.tar.gz" %(dbName)
-    print "-----------------------------"
-    print "Downloading %s" %(link)
-    call(["wget", link, "--no-check-certificate"])
-    print "Checking md5sum of %s database" %(dbName)
-    downloaded = Popen(["md5sum",tarName], stdout=PIPE)
-    checksum_downloaded = downloaded.communicate()[0].split()[0]
-    print checksum_downloaded
-        
-    if checksum_downloaded != md5sum:
-            print "DOWNLOAD of %s failed. Please re-run the script" %(link)
-            sys.exit(23)
-        
-        
-    cmd="tar -zxvf %s" %(tarName)
-    os.system(cmd)
-    os.remove(tarName)
+	else:
+		htmlName=dirDB+"/confirm"+dbName+".html"
+		getting = Popen(["wget", link, "--output-document="+htmlName, "-nv", "--keep-session-cookies", "--save-cookies", "./cookies.txt", "--no-check-certificate"])
+		if getting.wait():
+			print "problem downloading gdrive html file."
+		with open(htmlName, "r") as cfile:
+			code = cfile.read()
+			newlink = links[dbType][dbName][0] + "&confirm=" + re.search("confirm=(.{4})", code).group(1) + links[dbType][dbName][1]
+			getting = Popen(["wget", newlink, "--output-document="+tarName, "-nv", "--load-cookies", "./cookies.txt", "--no-check-certificate"])
+			if getting.wait():
+				print "problem downloading online database."
+		removehtml = Popen(["rm", "-f", htmlName])
+		if removehtml.wait():
+			print "problem deleting gdrive html page."
+	
+	print "Checking md5sum of %s database." %(dbName)
+	downloaded = Popen(["md5sum",tarName], stdout=PIPE)
+	checksum_downloaded = downloaded.communicate()[0].split()[0]
+	print checksum_downloaded
+	if checksum_downloaded != md5sum:
+		print "DOWNLOAD of %s failed. Checksum does not match. Please re-run the script." %(link)
+		sys.exit(23)
 
-
+	untar = Popen(["tar", "-zxvf", tarName])
+	if untar.wait():
+		print "problem extracting downloaded DB in TAR format."
+	removetar = Popen(["rm", tarName])
+	if removetar.wait():
+		print "problem deleting downloaded DB in TAR format."
+	
+def symlink(dirDB):
+	os.chdir(codeDir)
+	symlinks = Popen(["ln", "-s", dirDB, "./"])
+	if symlinks.wait():
+		print "problem creating link."
+	
 ####################################################################
 
 print "*********************************************"
-print "ROP (version 1.0.3) is a computational protocol aimed to discover the source of all reads, originated from complex RNA molecules, recombinant antibodies and microbial communities. Written by Serghei Mangul (smangul@ucla.edu) and Harry Taegyun Yang (harry2416@gmail.com), University of California, Los Angeles (UCLA). (c) 2016. Released under the terms of the General Public License version 3.0 (GPLv3)"
+print "ROP (version 1.0.6) is a computational protocol aimed to discover the source of all reads, originated from complex RNA molecules, recombinant antibodies and microbial communities. Written by Serghei Mangul (smangul@ucla.edu) and Harry Taegyun Yang (harry2416@gmail.com), University of California, Los Angeles (UCLA). (c) 2016. Released under the terms of the General Public License version 3.0 (GPLv3)"
 print ""
 print "For more details see:"
 print "https://sergheimangul.wordpress.com/rop/"
@@ -55,11 +138,12 @@ necessary_arguments.add_argument('dirDB', help='directory where the reference da
 
 
 input_option_arguments = ap.add_argument_group('Select Database (multi-selection is possible)')
+input_option_arguments.add_argument("--organism", help="downloads the (mouse) version of the database files", default="human")
 input_option_arguments.add_argument("--repeat", help="Set up database for repeat sequences ONLY (lost repeat reads) ", action="store_true")
 input_option_arguments.add_argument("--immune", help="Set up database for VDJ gene segments of B cell and T cell receptors ONLY (immune reads)", action="store_true")
-input_option_arguments.add_argument("--metaphlan", help = " Set up database for Metaphlan2 ONLY", action = "store_true")
-input_option_arguments.add_argument("--circRNA", help = "Set up database for circular RNAs ONLY", action="store_true")
-input_option_arguments.add_argument("--microbiome", help = "Set up database for microbime  ONLY", action = "store_true")
+input_option_arguments.add_argument("--metaphlan", help=" Set up database for Metaphlan2 ONLY", action="store_true")
+input_option_arguments.add_argument("--circRNA", help="Set up database for circular RNAs ONLY", action="store_true")
+input_option_arguments.add_argument("--microbiome", help="Set up database for microbiome ONLY", action="store_true")
 
 
 release = ap.add_argument_group('Connect database with the new release')
@@ -69,6 +153,9 @@ release.add_argument("--f", help="Reconnects the ROP to the new database. Please
 
 
 args = ap.parse_args()
+
+
+
 
 #=====================================================================================
 
@@ -80,138 +167,101 @@ targeted=False
 
 # IF none of them are selected: make everything true
 if not args.repeat and not args.immune and not args.circRNA and not args.microbiome and not args.metaphlan:
-    args.repeat = True
-    args.immune = True
-    args.circRNA = True
-    args.microbiome = True
-    args.metaphlan = True
+	args.repeat = True
+	args.immune = True
+	args.circRNA = True
+	args.microbiome = True
+	args.metaphlan = True
 else:
-    #It is gonna be non-reductive for now
-    targeted = True
+	#It is gonna be non-reductive for now
+	targeted = True
 
-
-
-
-dirDB=args.dirDB+"/db/"
-
-currentDB=codeDir+'/db'
-
-
-
+	
+#set organism parameters
+dbType=args.organism
+dirDB=args.dirDB+"/db_"+dbType
+currentDB=codeDir+"/db_"+dbType
 
 
 if os.path.exists(currentDB):
-    if args.f:
-        cmd='rm -f %s' %(currentDB)
-        os.system(cmd)
-    else:
-        print "Error : ROP was already linked to the reference database. To remove the current link please use --f option"
-        sys.exit(23)
+	if args.f:
+		removecurrDB = Popen(["rm", "-f", currentDB])
+		if removecurrDB.wait():
+			print "problem removing currentDB."
+	else:
+		print "Error : ROP was already linked to the reference database. To remove the current link please use --f option"
+		sys.exit(23)
 
-
-dict={}
-dict['rRNA']='3f85a14df97ad844e5175265e54148f5'
-dict['bowtie2Index']='5988f3f86b41252e005f78b6a022b7b8'
-dict['repeats']='34948f44b0a393228e93ce6bd2c4d512'
-dict['BWAIndex']='3de56482d4ec609cec03a9dbae8a0ecd'
-dict['metaphlan_db']='c0e06113269d1042c84b32abef99951c'
-dict['antibody']='8f2d8884677fe2e05736a3b92a57739b'
-dict['virus']='82980243db39b2d96bd2ffc633e40ce2'
-dict['eupathdb']='34c6ffb1f0d8664ba549ce45fefa8724'
-dict['bacteria']='19f09bfe965693ff9778e177c57a21bb'
-
-
+		
 if args.link2db:
-    os.chdir(codeDir)
-    
-    if os.path.exists(args.dirDB):
-        cmd="ln -s %s db" %(args.dirDB)
-        os.system(cmd)
-        print "Reference databases are connected. Please use rop.py"
-    else:
-        print "Error : Directory with reference databases doesn't exist! Please download the reference databases first. "
+	chdir2 = Popen(["cd", codeDir])
+	if chdir2.wait():
+		print "problem changing directories (the second time)."
+	if os.path.exists(dirDB):
+		dblink = Popen(["ln", "-s", dirDB, "/db_"+dbType])
+		if dblink.wait():
+			print "error linking database to ROP."
+		print "Reference databases are connected. Please use rop.py"
+	else:
+		print "Error : Directory with reference databases doesn't exist! Please download the reference databases first. "
 else:
-    if not os.path.exists(dirDB):
-        os.makedirs(dirDB)
-    else:
-        print "Error: Directory %s already exists. Please use  --link2db to connect the ROP with the reference databases " %(dirDB)
-        sys.exit(23)
+	if not os.path.exists(dirDB):
+		os.makedirs(dirDB)
+	else:
+		print "Error: Directory %s already exists. Please use  --link2db to connect the ROP with the reference databases." %(dirDB)
+		sys.exit(23)
 
 
 
 
 
 if not targeted:
-    
-    print "The complete refrence database will be downloaded"
-    
+	print "The complete reference database will be downloaded."
+	print "Downloading the database (~65GB) takes up to 45 minutes."
+	print "Please wait until the installation is completed."
 
 
+	for dbName in ['rRNA','bowtie2Index','repeats','BWAIndex','metaphlan_db','antibody','virus','eupathdb','bacteria']:
+		print "Downloading %s database files ..." %(dbName)
+		download(dbName,dbType,dirDB)
 
-
-    
-    
-    print "Downloading the database (~65GB) takes up to 45 minute"
-    print "Please wait until the installation is completed."
-
-
-
-
-
-    for dbName in ['rRNA','bowtie2Index','repeats','BWAIndex','metaphlan_db','antibody','virus','eupathdb','bacteria']:
-        print "Downloading %s database files ..." %(dbName)
-        download(dbName,dict[dbName],dirDB)
-
-        
-    os.chdir(codeDir)
-    cmd="ln -s %s ./" %(dirDB)
-    os.system(cmd)
-    print "Reference databases are ready. Please use rop.py"
+	symlink(dirDB)
+	print "Reference databases are ready. Please use rop.py"
 
 else:
-    
-    
-    if args.repeat:
-        for dbName in ['rRNA','bowtie2Index','repeats']:
-            print "Downloading %s database files ..." %(dbName)
-            download(dbName,dict[dbName],dirDB)
-        os.chdir(codeDir)
-        cmd="ln -s %s ./" %(dirDB)
-        os.system(cmd)
-        print "Reference databases are ready. Please use rop.py"
-
-    if args.immune:
-        for dbName in ['rRNA','bowtie2Index','antibody']:
-            print "Downloading %s database files ..." %(dbName)
-            download(dbName,dict[dbName],dirDB)
-        os.chdir(codeDir)
-        cmd="ln -s %s ./" %(dirDB)
-        os.system(cmd)
-        print "Reference databases are ready. Please use rop.py"
-    if args.metaphlan:
-        for dbName in ['rRNA','bowtie2Index','metaphlan_db']:
-            print "Downloading %s database files ..." %(dbName)
-            download(dbName,dict[dbName],dirDB)
-        os.chdir(codeDir)
-        cmd="ln -s %s ./" %(dirDB)
-        os.system(cmd)
-        print "Reference databases are ready. Please use rop.py"
-    if args.circRNA:
-        for dbName in ['rRNA','bowtie2Index','BWAIndex']:
-            print "Downloading %s database files ..." %(dbName)
-            download(dbName,dict[dbName],dirDB)
-        os.chdir(codeDir)
-        cmd="ln -s %s ./" %(dirDB)
-        os.system(cmd)
-        print "Reference databases are ready. Please use rop.py"
-    if args.microbiome:
-        print "--mirobiome options was selected. Refrence database for microbiome will be downloaded"
-        for dbName in ['bacteria','rRNA','bowtie2Index','virus','eupathdb']:
-            print "Downloading %s database files ..." %(dbName)
-            download(dbName,dict[dbName],dirDB)
-        os.chdir(codeDir)
-        cmd="ln -s %s ./" %(dirDB)
-        os.system(cmd)
-        print "Reference databases are ready. Please use rop.py"
 
 
+	if args.repeat:
+		for dbName in ['rRNA','bowtie2Index','repeats']:
+			print "Downloading %s database files ..." %(dbName)
+			download(dbName,dbType,dirDB)
+		symlink(dirDB)
+		print "Reference databases are ready. Please use rop.py"
+
+	if args.immune:
+		for dbName in ['rRNA','bowtie2Index','antibody']:
+			print "Downloading %s database files ..." %(dbName)
+			download(dbName,dbType,dirDB)
+		symlink(dirDB)
+		print "Reference databases are ready. Please use rop.py"
+		
+	if args.metaphlan:
+		for dbName in ['rRNA','bowtie2Index','metaphlan_db']:
+			print "Downloading %s database files ..." %(dbName)
+			download(dbName,dbType,dirDB)
+		symlink(dirDB)
+		print "Reference databases are ready. Please use rop.py"
+	if args.circRNA:
+		for dbName in ['rRNA','bowtie2Index','BWAIndex']:
+			print "Downloading %s database files ..." %(dbName)
+			download(dbName,dbType,dirDB)
+		symlink(dirDB)
+		print "Reference databases are ready. Please use rop.py"
+		
+	if args.microbiome:
+		print "--microbiome options was selected. Refrence database for microbiome will be downloaded"
+		for dbName in ['bacteria','rRNA','bowtie2Index','virus','eupathdb']:
+			print "Downloading %s database files ..." %(dbName)
+			download(dbName,dbType,dirDB)
+		symlink(dirDB)
+		print "Reference databases are ready. Please use rop.py"
